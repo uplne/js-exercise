bf950ce564c101a7899ad747ded57317e88420e7
Steve Vadocz <uplneinak@gmail.com>
2014-04-22 21:52:06 +0100

    Added logical operators

--- tests/specs/arrays.js
+++ tests/specs/arrays.js
@@ -1,11 +1,11 @@
-'use strict';
-
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 if (typeof expect !== 'function') { var expect = require('expect.js'); }
 
 define([
     '../../exams/arrays'
 ], function(exam) {
+    'use strict';
+
     describe('Arrays', function() {
         var arr = [];
 
---
+++ tests/specs/logicalOperators.js
@@ -0,0 +1,12 @@
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof expect !== 'function') { var expect = require('expect.js'); }
+
+define([
+    '../../exams/arrays'
+], function(exam) {
+    'use strict';
+
+    describe('LogicalOperators', function() {
+
+    });
+});
